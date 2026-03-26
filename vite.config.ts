69bc5e0830caf0d52b25b62a5db6072a2b724b64
cjOne7 <nikyarosh07@gmail.com>
2026-03-26 23:08:55 +0100

    feat: configure files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import babel from "vite-plugin-babel";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      babelConfig: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
   resolve: {
     tsconfigPaths: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
